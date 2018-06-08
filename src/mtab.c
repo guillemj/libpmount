1680311e50c411fe00cb02c4ac2c7a1e3ff1aae2
Guillem Jover <guillem@hadrons.org>
2018-06-09 00:03:50 +0200

    mtab: Completely rework mtab handling
    The code was just broken:
    
    - was doing replaces by moving the mtab aside, instead of atomic and
      durable replaces (with link(), fsync() and rename()).
    - using hardcoded line buffer with fgets(), instead of getline().
    - leaking resources on error and success conditions.
    - not initializing resources to avoid releasing garbage.
    - not checking whether resources had already been released.
    
    Warned-by: coverity

--- src/mtab.c
+++ src/mtab.c
@@ -76,77 +76,101 @@ __mtab_add(char *line)
 int
 __mtab_del(char *mntdir)
 {
-  FILE *mtab_r, *mtab_w;
-  char *mtab_path_new;
-  char line[8192]; /* Yeah; I know this is lame, but it works.
-                      Patches welcome. */
-  char *ret;
+  FILE *mtab_r = NULL;
+  FILE *mtab_w = NULL;
+  char *mtab_path_new = NULL;
+  char *mntdir_str = NULL;
+  char *line = NULL;
+  size_t line_len = 0;
+  int ret = PMOUNT_NOTME;
   int rc;
 
   if (asprintf(&mtab_path_new, "%s~", __mtab_path) < 0)
   {
     perror("asprintf");
-    return PMOUNT_NOTME;
+    goto cleanup_release;
   }
 
-  if (rename(__mtab_path, mtab_path_new) == -1)
-    if (errno != ENOENT)
-    {
-      perror("rename");
-      return PMOUNT_NOTME;
-    }
-  mtab_r = fopen(mtab_path_new, "r");
+  mtab_r = fopen(__mtab_path, "r");
   if (mtab_r == NULL)
   {
     perror("fopen");
-    return PMOUNT_NOTME;
+    goto cleanup_release;
   }
-  mtab_w = fopen(__mtab_path, "w");
+  mtab_w = fopen(mtab_path_new, "w");
   if (mtab_w == NULL)
   {
     perror("fopen");
-    return PMOUNT_NOTME;
+    goto cleanup_release;
   }
 
-  rc = asprintf(&mntdir, " %s ", mntdir);
+  rc = asprintf(&mntdir_str, " %s ", mntdir);
   if (rc < 0)
   {
     perror("asprintf");
-    return PMOUNT_NOTME;
+    goto cleanup_unlink;
   }
+
   while (1)
   {
-    ret = fgets(line, sizeof(line), mtab_r);
-    if (ret == NULL)
+    ssize_t nread;
+
+    nread = getline(&line, &line_len, mtab_r);
+    if (nread < 0)
     {
       if (errno == 0)
         break;
-      perror("fgets");
-      return PMOUNT_NOTME;
+      perror("getline");
+      goto cleanup_unlink;
     }
-    if (strstr(line, mntdir) == NULL)
-      fprintf(mtab_w, "%s", ret);
+    if (strstr(line, mntdir_str) == NULL)
+      fprintf(mtab_w, "%s", line);
   }
-  free(mntdir);
+
+  if (fsync(fileno(mtab_w)) < 0)
+  {
+    perror("fsync");
+    goto cleanup_unlink;
+  }
 
   if (fclose(mtab_r) != 0)
   {
     perror("fclose");
-    return PMOUNT_NOTME;
+    mtab_r = NULL;
+    goto cleanup_unlink;
   }
+  mtab_r = NULL;
   if (fclose(mtab_w) != 0)
   {
     perror("fclose");
-    return PMOUNT_NOTME;
+    mtab_w = NULL;
+    goto cleanup_unlink;
   }
-  if (unlink(mtab_path_new) != 0)
+  mtab_w = NULL;
+  if (rename(mtab_path_new, __mtab_path) != 0)
   {
-    perror("unlink");
-    return PMOUNT_NOTME;
+    perror("rename");
+    goto cleanup_unlink;
   }
+
+  ret = 0;
+  goto cleanup_release;
+
+cleanup_unlink:
+  if (unlink(mtab_path_new) != 0)
+    perror("unlink");
+
+cleanup_release:
+  if (mtab_r != NULL && fclose(mtab_r) != 0)
+    perror("fclose");
+  if (mtab_w != NULL && fclose(mtab_w) != 0)
+    perror("fclose");
+
+  free(line);
+  free(mntdir_str);
   free(mtab_path_new);
 
-  return 0;
+  return ret;
 }
 
 /* Searches mtab for a line using 'mntdir'. If successful, returns the whole
@@ -154,10 +178,11 @@ __mtab_del(char *mntdir)
 char *
 __mtab_getline(char *mntdir)
 {
-  FILE *mtab;
-  char line[8192]; /* Yeah; I know this is lame, but it works.
-                      Patches welcome. */
-  char *ret;
+  FILE *mtab = NULL;
+  char *mntdir_str;
+  char *line;
+  size_t line_len;
+  char *ret = NULL;
 
   mtab = fopen(__mtab_path, "r");
   if (mtab == NULL)
@@ -166,20 +191,24 @@ __mtab_getline(char *mntdir)
     return NULL;
   }
 
-  if (asprintf(&mntdir, " %s ", mntdir) < 0)
+  if (asprintf(&mntdir_str, " %s ", mntdir) < 0)
   {
     perror("asprintf");
-    return NULL;
+    mntdir_str = NULL;
+    goto cleanup;
   }
   while (1)
   {
-    ret = fgets(line, sizeof(line), mtab);
-    if (ret == NULL)
+    ssize_t nread;
+
+    nread = getline(&line, &line_len, mtab);
+    if (nread < 0)
     {
       if (errno == 0)
         break;
+
       perror("fgets");
-      return NULL;
+      goto cleanup;
     }
     if (strstr(line, mntdir) != NULL)
     {
@@ -187,13 +216,12 @@ __mtab_getline(char *mntdir)
       break;
     }
   }
-  free(mntdir);
 
-  if (fclose(mtab) != 0)
-  {
+cleanup:
+  free(mntdir_str);
+
+  if (mtab != NULL && fclose(mtab) != 0)
     perror("fclose");
-    return NULL;
-  }
 
   return ret;
 }
